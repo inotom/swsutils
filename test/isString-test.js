a3277c4a7441455d3e51c385fc6ce5c0e62238c3
inotom <wdf7322@yahoo.co.jp>
2016-07-01 11:30:06 +0900

    Add isString testing

---
+++ test/isString-test.js
@@ -0,0 +1,21 @@
+import assert from 'power-assert';
+import isString from '../src/isString';
+
+describe('isString', function() {
+
+  it('string value', function() {
+    assert(isString('') === true);
+    assert(isString('hello') === true);
+  });
+
+  it('not string value', function() {
+    assert(isString(null) === false);
+    assert(isString(void(0)) === false);
+    assert(isString(0) === false);
+    assert(isString(1.0) === false);
+    assert(isString({}) === false);
+    assert(isString({foo:''}) === false);
+    assert(isString([]) === false);
+    assert(isString([0]) === false);
+  });
+});
